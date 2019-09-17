77f5be6b23bbf8c08d003c6e59b008e249efdfd0
Nicholas Sturtzel <gigasturtz@Nicholass-MacBook-Pro-7.local>
2019-09-16 22:13:34 -0400

    writes cardData to json file

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+async function getURIFromScryfall() {
+    const cardUri = await fetch('https://api.scryfall.com/bulk-data').then(response => response.json()).then(data => data.data[0].permalink_uri)
+    return cardUri
+}
+
+async function getCardsFromCardUri() {
+    const url = await getURIFromScryfall()
+    fetch(url).then(r => r.json()).then(j => fs.writeFile("cardData.json", JSON.stringify(j), function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
+                    
+                        console.log("The file was saved!")
+                    }) )
+}
+getCardsFromCardUri()
